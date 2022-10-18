56ede1d3f50644d8ab930f027abcc39cb055bbb3
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2022-10-19 06:14:55 +0900

    refacter

--- components/ui/VideoPlayer/VideoPlayer.tsx
+++ components/ui/VideoPlayer/VideoPlayer.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react'
-import { useCanPlayMovie } from '@components/context'
 
 interface Props {
     mp4: string
@@ -9,7 +8,7 @@ interface Props {
 }
 
 const VideoPlayer: FC<Props> = ({ mp4, webm, poster, onPlay }: Props) => {
-    const { isPlaying } = useCanPlayMovie()
+
     return (
         <video autoPlay loop muted playsInline onPlay={onPlay} style={{ objectFit:"cover", height:"100%", width:"100%" }} poster={poster} className='absolute w-full h-screen'>
             <source src={mp4} type='video/mp4; codecs=hvc1' />
