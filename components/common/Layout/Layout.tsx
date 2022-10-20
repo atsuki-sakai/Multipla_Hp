5e1c8601129c2243a2f12adb23e0ffaa20ad9574
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2022-10-21 06:44:03 +0900

    refacter

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -45,14 +45,14 @@ const Layout: FC<Props> = ({ children }: Props) => {
     }, [isLoaded])
     return (
         <div className='relative'>
-            <motion.div initial={{ x:0 }} animate={{ x: isLoaded ? "-100%": "0" }} transition={{ duration:1, ease: "easeInOut" }} className="absolute top-0 left-0 h-screen w-screen z-50">
-                <div className="bg-black h-full w-full flex justify-center items-center">
+            <motion.div initial={{ x:0, opacity:1 }} animate={{ x: isLoaded ? "-100%": "0", opacity: isLoaded ? 0: 1 }} transition={{ duration:1, ease: "easeInOut" }} className="absolute top-0 left-0 h-screen w-screen z-50">
+                <div className="bg-gradient-to-bl to-purple-400 from-red-300 h-full w-full flex justify-center items-center">
                     <div className='animate-pulse'>
                         <div className="flex items-center justify-center mb-12">
                             <div className="relative h-10 w-10">
                                 <Image src={"/images/test-logo.png"} layout="responsive" height={80} width={80} alt={"Logo"}></Image>
                             </div>
-                            <div className="text-white">
+                            <div className='text-gray-700 font-noto_sans'>
                                 <h3 className="text-sm">MULTIPLA</h3>
                                 <p className="text-[10px] scale-75 -translate-x-2">マルチプラ</p>
                             </div>
