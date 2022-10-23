24088552fd2a78ddeb9e66bad0a883f2019e37dd
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2022-10-23 10:16:22 +0900

    refacter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,13 @@
 import { Html, Head, Main, NextScript,  } from "next/document";
 
+// TODO: - google-site-verificationは必要ない？
+
 const Document = () => {
     return (
         <Html lang="ja">
         <Head>
-            {/* <meta name="google-site-verification" content="BsIr2ery5Dz60HVmcw6jA4YKCqqLfYEUB0XL13fSs0Y" />
-                    <meta name="facebook-domain-verification" content="2wgqgx6iqxx9bznrd899j3apfcbkrl" /> */}
+            <meta name="google-site-verification" content="3Bp4yAh5tu2HNgZYIxu-QrEu9yjF3yg0H47-Cyox3_Y" />
+            <meta name="facebook-domain-verification" content="xqc29930rhh4hvlr5uehc2ua23j1om" />
         </Head>
         <body>
             <Main />
