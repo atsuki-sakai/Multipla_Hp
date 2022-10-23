568e75b8dadcf2f625c7e8e747f2753ec3b4bc96
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2022-10-23 10:07:52 +0900

    refacter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import React, { useEffect } from "react"
+import { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
 import { Layout } from "@components/common"
 import { UIProvider} from "@components/context"
@@ -7,9 +8,21 @@ import TagManager from "react-gtm-module"
 
 function MyApp({ Component, pageProps }: AppProps) {
 
+  const router = useRouter()
+
   useEffect(() => {
     TagManager.initialize({ gtmId: String(process.env.NEXT_PUBLIC_GTM_MANAGER_ID) })
-  }, [])
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init(String(process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID)) // facebookPixelId
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
 
   return  <UIProvider>
             <Layout>
